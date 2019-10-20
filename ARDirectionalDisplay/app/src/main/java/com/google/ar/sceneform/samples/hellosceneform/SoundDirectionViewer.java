cf023f148ab517d43abd740f02cfd5faa47a043f
Keerthan Ekbote <saiskee@gmail.com>
2019-10-20 04:45:32 -0400

    Commited AR Directional Display Software

---
+++ ARDirectionalDisplay/app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/SoundDirectionViewer.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.ar.sceneform.samples.hellosceneform;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.java_websocket.client.WebSocketClient;
+
+/**
+ * This is an example activity that uses the Sceneform UX package to make common AR tasks easier.
+ */
+public class SoundDirectionViewer extends AppCompatActivity {
+
+  private static final String TAG = SoundDirectionViewer.class.getSimpleName();
+  private static final double MIN_OPENGL_VERSION = 3.0;
+
+private static int frameCounter =0;
+private static float theta = 0;
+  private static int andyModelNumbers = 0;
+  private ArFragment arFragment;
+
+  private ModelRenderable andyRenderable;
+  private TransformableNode andyNode;
+  private AnchorNode anchorNode;
+  private WebSocketClient client;
+
+  protected void backgroundNetworking() {
+      new Thread() {
+          public void run() {
+              HttpURLConnection urlConnection = null;
+              while (true || false) {
+                  try{
+                      URL url = new URL("http://10.42.0.208:3111");
+                      urlConnection = (HttpURLConnection) url.openConnection();
+                      urlConnection.setRequestMethod("GET");
+                      urlConnection.connect();
+                      InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                      BufferedReader br = new BufferedReader(new InputStreamReader(
+                              (urlConnection.getInputStream())));
+                      String output;
+                      while ((output = br.readLine()) != null) {
+
+                          String[] components = output.split(",");
+                          if (components.length > 0) {
+                              theta = Float.valueOf(components[0]);
+                          }
+
+
+                      }
+                      Thread.sleep(100);
+
+                  } catch (Exception e) {
+                      System.out.println("Error is: " + e);
+                  } finally {
+                      if (urlConnection != null) {
+                          urlConnection.disconnect();
+                      }
+                  }
+              }
+
+          }
+      }.start();
+  }
+
+  @Override
+  @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+  // CompletableFuture requires api level 24
+  // FutureReturnValueIgnored is not valid
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      if (!checkIsSupportedDeviceOrFinish(this)) {
+          return;
+      }
+
+      setContentView(R.layout.activity_ux);
+      arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+      // When you build a Renderable, Sceneform loads its resources in the background while returning
+      // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+      ModelRenderable.builder()
+              .setSource(this, R.raw.arrow)
+              .build()
+              .thenAccept(renderable -> andyRenderable = renderable)
+              .exceptionally(
+                      throwable -> {
+                          Toast toast =
+                                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                          toast.setGravity(Gravity.CENTER, 0, 0);
+                          toast.show();
+                          return null;
+                  });
+
+
+      arFragment.setOnTapArPlaneListener((HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+
+          if (andyRenderable == null) {
+              return;
+          }
+
+          // Create the Anchor.
+          Anchor anchor = hitResult.createAnchor();
+          anchorNode = new AnchorNode(anchor);
+          anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+          if (andyModelNumbers < 1) {
+              // Create the transformable andy and add it to the anchor.
+
+              andyNode = new TransformableNode(arFragment.getTransformationSystem()){
+                  @Override
+                  public void onUpdate(FrameTime frameTime) {
+                      super.onUpdate(frameTime);
+                      andyNode.setWorldRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), (float) Math.toDegrees((double) theta)));
+                  }
+              };
+
+              andyNode.setParent(anchorNode);
+              andyNode.setRenderable(andyRenderable);
+              andyNode.select();
+
+              andyModelNumbers++;
+              backgroundNetworking();
+          }
+      });
+  }
+
+
+  /**
+   * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+   * on this device.
+   *
+   * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+   *
+   * <p>Finishes the activity if Sceneform can not run
+   */
+  public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+    if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+      Log.e(TAG, "Sceneform requires Android N or later");
+      Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+      activity.finish();
+      return false;
+    }
+    String openGlVersionString =
+        ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+            .getDeviceConfigurationInfo()
+            .getGlEsVersion();
+    if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+      Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+      Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+          .show();
+      activity.finish();
+      return false;
+    }
+    return true;
+  }
+}
